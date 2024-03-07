33c079ad4ee8c91b485b1496b47c424f3acaaa4f
spectre940105 <128953799+spectre940105@users.noreply.github.com>
2024-03-07 09:46:44 +0800

    Add files via upload

--- 第二次作業.py
+++ 第二次作業.py
@@ -1,8 +1,8 @@
 #圓面積＆圓周長
 
-a=float(input("請輸入半徑："))
-print("圓周長：",a*2*3.14)
-print("圓面積：",a*a*3.14)
+radius=float(input("請輸入半徑："))
+print("圓周長：",radius*2*3.14)
+print("圓面積：",radius*radius*3.14)
 
 #字典
 
@@ -10,16 +10,20 @@ name=str(input("請輸入名字："))
 age=int(input("請輸入年齡："))
 height=float(input("請輸入身高(M)："))
 color=str(input("請輸入喜歡的顏色："))
-material={'name':name,'age':age,'height':height,'color':color}
-print(material['name'],',',material['age'],'歲,身高',material['height'],'米,喜歡的顏色是',material['color'])
+material={'姓名':name,'年齡':age,'身高':height,'顏色':color}
+output=f'{material["姓名"]},{material["年齡"]}歲,身高{material["身高"]}米,最喜歡的顏色{material["顏色"]}'
+print(output)
 
 #判斷偶數
-
-a1=input("請輸入整數：")
-if(type(a1)==str):
-    print("請輸入整數，不是字串or空字符串")
-elif(a1=='int'):
-    if(a1%2==0):
-        print("偶數")
-    else:
-        print("基數")
+while True:
+    try:
+        number=input("請輸入整數:")
+        number=int(number)
+        if(number%2==0):
+            print(f"{number}是偶數")
+        else:
+            print(f"{number}是基數")
+        break
+    except ValueError:
+        print("錯誤,請輸入整數")
+    
